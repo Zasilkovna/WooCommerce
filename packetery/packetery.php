39872b4f3b2f0acefa5f47e02ba60a74d4a6a4af
Jan Potužník <jan.potuznik@packeta.com>
2021-07-19 12:46:57 +0200

    PES-246: Shipping method v2

--- packetery/packetery.php
+++ packetery/packetery.php
@@ -133,27 +133,30 @@ $configurator->createRobotLoader()
 $container = $configurator->createContainer();
 $container->getByType(\Packetery\Plugin::class)->run();
 
+/**
+ * @link https://docs.woocommerce.com/document/shipping-method-api/
+ */
 function packetery_shipping_method_init() {
 	// Your class will go here
-	if (!class_exists('WC_Packetery_Shipping_Method')) {
+	if ( ! class_exists( 'WC_Packetery_Shipping_Method' ) ) {
 
-		class WC_Packetery_Shipping_Method extends WC_Shipping_Method
-		{
+		class WC_Packetery_Shipping_Method extends WC_Shipping_Method {
 			/**
 			 * Constructor for Packeta shipping class
 			 *
 			 * @access public
+			 *
 			 * @param int $instance_id
 			 */
-			public function __construct( $instance_id = 0 ) {
+			public function __construct( int $instance_id = 0 ) {
 				parent::__construct();
-				$this->id = 'packetery_shipping_method';
-				$this->instance_id  = absint( $instance_id );
-				$this->method_title = __( 'Packeta Shipping Method', 'packetery' );
-				$this->title = __('Packeta Shipping Method');
-				$this->method_description = __('Description of Packeta shipping method'); //
-				$this->enabled = "yes"; // This can be added as an setting but for this example its forced enabled
-				$this->supports = array(
+				$this->id                 = 'packetery_shipping_method';
+				$this->instance_id        = absint( $instance_id );
+				$this->method_title       = __( 'Packeta Shipping Method', 'packetery' );
+				$this->title              = __( 'Packeta Shipping Method' );
+				$this->method_description = __( 'Description of Packeta shipping method' ); //
+				$this->enabled            = 'yes'; // This can be added as an setting but for this example its forced enabled
+				$this->supports           = array(
 					'shipping-zones',
 				);
 				$this->init();
@@ -165,49 +168,43 @@ function packetery_shipping_method_init() {
 			 * @access public
 			 * @return void
 			 */
-			public function init() {
+			public function init(): void {
 				// todo Load the settings API
 				//$this->init_form_fields(); // This is part of the settings API. Override the method to add your own settings
 				//$this->init_settings(); // This is part of the settings API. Loads settings you previously init.
 
 				// Save settings in admin if you have any defined
-				add_action('woocommerce_update_options_shipping_' . $this->id, array($this, 'process_admin_options'));
+				add_action( 'woocommerce_update_options_shipping_' . $this->id, array(
+					$this,
+					'process_admin_options'
+				) );
 			}
 
 			/**
 			 * calculate_shipping function.
 			 *
 			 * @access public
-			 * @param mixed $package
+			 *
+			 * @param array $package
+			 *
 			 * @return void
 			 */
-			public function calculate_shipping($package = array()) {
-				/* todo
-				$defaults = array(
-					'label' => '',   // Label for the rate
-					'cost' => '0',  // Amount for shipping or an array of costs (for per item shipping)
-					'taxes' => '',   // Pass an array of taxes, or pass nothing to have it calculated for you, or pass 'false' to calculate no tax for this method
-					'calc_tax' => 'per_order' // Calc tax per_order or per_item. Per item needs an array of costs passed via 'cost'
-				);
-				*/
-
-				// This is where you'll add your rates
+			public function calculate_shipping( $package = array() ): void {
+				// TODO: This is where you'll add your rates
 				$rate = array(
-					'label' => "Label for the rate",
-					'cost' => '70',
-					'calc_tax' => 'per_item'
+					'label'    => 'Packeta shipping rate',
+					'cost'     => 0,
+					'taxes'    => '',
+					'calc_tax' => 'per_order'
 				);
 
 				// Register the rate
-				$this->add_rate($rate);
+				$this->add_rate( $rate );
 			}
 		}
 	}
 }
-add_action('woocommerce_shipping_init', 'packetery_shipping_method_init');
 
-function packetery_add_shipping_method($methods) {
-	$methods['packetery_shipping_method'] = WC_Packetery_Shipping_Method::class;
-	return $methods;
-}
-add_filter('woocommerce_shipping_methods', 'packetery_add_shipping_method');
+add_action( 'woocommerce_shipping_init', 'packetery_shipping_method_init' );
+
+add_filter( 'woocommerce_shipping_methods', array( '\Packetery\Plugin', 'add_shipping_method' ) );
