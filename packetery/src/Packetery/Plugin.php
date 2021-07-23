fdb59344b0260d6517be88db3384dc20f7b6e3fe
Jan Potužník <jan.potuznik@packeta.com>
2021-07-23 13:57:43 +0200

    PES-246: bugfixes

--- packetery/src/Packetery/Plugin.php
+++ packetery/src/Packetery/Plugin.php
@@ -11,8 +11,13 @@ class Plugin {
 		// register hooks
 	}
 
-	public static function add_shipping_method( $methods ) {
-		$methods['packetery_shipping_method'] = WC_Packetery_Shipping_Method::class;
+	/**
+	 * @param array $methods
+	 *
+	 * @return array
+	 */
+	public static function add_shipping_method( array $methods ): array {
+		$methods['packetery_shipping_method'] = \WC_Packetery_Shipping_Method::class;
 
 		return $methods;
 	}
