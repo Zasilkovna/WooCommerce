a82c434e7fe7fbec64b81814ccbd6750108d67ac
Filip Jiskra <filip.jiskra@zasilkovna.cz>
2021-07-30 12:26:34 +0200

    PES-251: packeta-options page - translations base

--- packetery/cli/create-latte-cache.php
+++ packetery/cli/create-latte-cache.php
@@ -1,10 +1,21 @@
 <?php
+/**
+ * Creates latte cache. Use before generating pot file.
+ *
+ * @package Packetery
+ */
 
-$container = require __DIR__ . '/../bootstrap-cli.php';
-/** @var \Latte\Engine $latteEngine */
-$latteEngine = $container->getByType(\Latte\Engine::class);
+$container    = require __DIR__ . '/../bootstrap-cli.php';
+$latte_engine = $container->getByType( \Latte\Engine::class );
 
-$finder = \Nette\Utils\Finder::findFiles('*.latte')->from(PACKETERY_PLUGIN_DIR . '/template');
+if ( is_dir( $container->parameters['latteTempFolder'] ) ) {
+	$files_to_delete = \Nette\Utils\Finder::findFiles( '*' )->from( $container->parameters['latteTempFolder'] );
+	foreach ( $files_to_delete as $file_to_delete ) {
+		unlink( $file_to_delete );
+	}
+}
+
+$finder = \Nette\Utils\Finder::findFiles( '*.latte' )->from( PACKETERY_PLUGIN_DIR . '/template' );
 foreach ( $finder as $file ) {
-	$latteEngine->warmupCache($file);
+	$latte_engine->warmupCache( $file );
 }
