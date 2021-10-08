adc9388e676401cfdb3356b671c082bce3b8114f
Jan Potužník <jan.potuznik@packeta.com>
2021-10-08 13:15:44 +0200

    PES-367: readme.txt

--- packetery/packetery.php
+++ packetery/packetery.php
@@ -7,18 +7,15 @@
  * @wordpress-plugin
  *
  * Plugin Name: Packeta
- * Plugin URI:
- * Description: Todo description.
+ * Description: With the help of our official plugin, You can choose pickup points of Packeta and it's external carriers in all of Europe, or utilize address delivery for 25 countries in the European Union, straight from the cart in Your eshop. You can submit all of Your orders to Packeta with one click.
  * Version: 1.0.0
- * Author: Packeta
- * Author URI: https://www.packeta.com/
- * Developer: Packeta
- * Developer URI: https://www.packeta.com/
+ * Author: Zásilkovna s.r.o.
+ * Author URI: https://www.zasilkovna.cz/
  * Text Domain: packetery
  * Domain Path: /languages
  *
- * WC requires at least: 5.0
- * WC tested up to: 5.5.0
+ * WC requires at least: 4.5
+ * WC tested up to: 5.7.1
  *
  * License: GNU General Public License v3.0
  * License URI: http://www.gnu.org/licenses/gpl-3.0.html
