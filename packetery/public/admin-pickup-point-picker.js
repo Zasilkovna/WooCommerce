99f97b11e7ca9b5e66dc49f65861e64e19b38b99
FJiskra <filip.jiskra@gmail.com>
2021-11-30 09:48:29 +0100

    Pes 378 admin vm picker (#112)
    * PES-378: admin PP picker
    
    * PES-378: admin PP picker - checkout PP attrs integration
    
    * PES-378: admin PP picker - button small styling
    
    * PES-378: admin PP picker - button bigger styling
    
    * PES-378: admin PP picker - button styling with packeta logo
    
    * PES-378: admin PP picker - sniffer
    
    * PES-378: admin PP picker - CR
    
    Co-authored-by: Filip Jiskra <filip.jiskra@zasilkovna.cz>

---
+++ packetery/public/admin-pickup-point-picker.js
@@ -0,0 +1,48 @@
+var packeteryLoadPickupPointPicker = function( $, settings ) {
+	var init = function( settings ) {
+		var $widgetDiv = $( '[data-packetery-order-metabox]' );
+
+		$widgetDiv.on( 'click', '[name=packetery_pick_pickup_point]', function( e ) {
+			e.preventDefault();
+
+			var widgetOptions = {
+				country: settings.country,
+				language: settings.language,
+				appIdentity: settings.appIdentity,
+				weight: settings.weight,
+				carriers: settings.carriers
+			};
+
+			Packeta.Widget.pick( settings.packeteryApiKey, function( point ) {
+				if ( point == null ) {
+					return;
+				}
+
+				for ( var attrKey in settings.pickupPointAttrs ) {
+					if ( !settings.pickupPointAttrs.hasOwnProperty( attrKey ) ) {
+						continue;
+					}
+
+					var attr = settings.pickupPointAttrs[ attrKey ];
+
+					$widgetDiv.find( '[name=' + attr.name + ']' ).val( point[ attrKey ] || '' );
+				}
+
+				$widgetDiv.find( '[data-packetery-widget-info]' ).html( point.name );
+			}, widgetOptions );
+		} );
+	};
+
+	var dependencies = [];
+	dependencies.push( $.getScript( "https://widget.packeta.com/v6/www/js/library.js" ) );
+
+	dependencies.push(
+		$.Deferred( function( deferred ) {
+			$( deferred.resolve ); // wait for DOM to be loaded
+		} )
+	);
+
+	$.when.apply( null, dependencies ).done( function() {
+		init( settings );
+	} );
+};
