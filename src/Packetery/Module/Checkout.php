8bb35717e820614f8a1e7fa42b0819e848d424d5
Filip Jiskra <filip.jiskra@zasilkovna.cz>
2022-03-10 16:06:58 +0100

    Revert "PES-615: shipping address replacing - logic extraction"
    This reverts commit 066f747adf9cf96a23719229515a09666d4b1b09.

--- src/Packetery/Module/Checkout.php
+++ src/Packetery/Module/Checkout.php
@@ -375,7 +375,7 @@ class Checkout {
 		}
 
 		if ( $this->isPickupPointOrder() ) {
-			$moduleOrder = new Entity( wc_get_order( $orderId ) );
+			$wcOrder = wc_get_order( $orderId );
 
 			foreach ( self::$pickupPointAttrs as $attr ) {
 				$attrName = $attr['name'];
@@ -396,11 +396,20 @@ class Checkout {
 				}
 
 				if ( $this->options_provider->replaceShippingAddressWithPickupPointAddress() ) {
-					$moduleOrder->updateShippingAddressAttribute( $attrName, $attrValue );
+					if ( Entity::META_POINT_STREET === $attrName ) {
+						$wcOrder->set_shipping_address_1( $attrValue );
+						$wcOrder->set_shipping_address_2( '' );
+					}
+					if ( Entity::META_POINT_CITY === $attrName ) {
+						$wcOrder->set_shipping_city( $attrValue );
+					}
+					if ( Entity::META_POINT_ZIP === $attrName ) {
+						$wcOrder->set_shipping_postcode( $attrValue );
+					}
 				}
 			}
 
-			$moduleOrder->save();
+			$wcOrder->save();
 		}
 
 		$orderEntity = new Core\Entity\Order( (string) $orderId, $carrierId );
