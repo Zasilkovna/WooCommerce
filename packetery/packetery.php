5deaf86a0a08ff839ccb6e55e8ce5ecc97aff00a
MatyasKoval <92293068+MatyasKoval@users.noreply.github.com>
2022-03-22 13:26:51 +0100

    Update packetery.php
    Updated WC and WP versions + slight description change

--- packetery/packetery.php
+++ packetery/packetery.php
@@ -7,17 +7,16 @@
  * @wordpress-plugin
  *
  * Plugin Name: Packeta
- * Description: With the help of our official plugin, You can choose pickup points of Packeta and it's external carriers in all of Europe, or utilize address delivery for 25 countries in the European Union, straight from the cart in Your eshop. You can submit all of Your orders to Packeta with one click.
+ * Description: With the help of our official plugin, You can choose pickup points of Packeta and its external carriers in all of Europe, or utilize address delivery to 25 countries in the European Union, straight from the cart in Your e-shop. You can also submit all your orders to Packeta with just one click.
  * Version: 1.2.0
  * Author: Zásilkovna s.r.o.
  * Author URI: https://www.zasilkovna.cz/
  * Text Domain: packetery
  * Domain Path: /languages
- * Requires at least: 5.3
+ * Requires at least: WordPress 5.3, WooCommerce 4.5
+ * Tested up to: WordPress 5.9.2, WooCommerce 6.3.1
  * Requires PHP: 7.2
  *
- * WC requires at least: 4.5
- * WC tested up to: 6.1.1
  *
  * License: GNU General Public License v3.0
  * License URI: http://www.gnu.org/licenses/gpl-3.0.html
