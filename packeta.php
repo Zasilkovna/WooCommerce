5c1eb4be9de2f18c5e94b1f614001b2c5948d856
Oldrich Korec Mraz <oldrich.mraz@packeta.com>
2022-05-11 13:31:37 +0200

    PES-790 Change TextDomain

--- packeta.php
+++ packeta.php
@@ -11,7 +11,7 @@
  * Version: 1.2.6
  * Author: Zásilkovna s.r.o.
  * Author URI: https://www.zasilkovna.cz/
- * Text Domain: packetery
+ * Text Domain: packeta
  * Domain Path: /languages
  * Requires at least: 5.3
  * Requires PHP: 7.2
