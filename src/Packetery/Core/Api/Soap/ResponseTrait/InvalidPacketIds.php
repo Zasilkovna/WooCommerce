24b8857b69234d22a9602db71830e9feb0c9c6e1
Filip Jiskra <filip.jiskra@zasilkovna.cz>
2022-06-07 13:24:32 +0200

    PES-781: log for each order - core update - InvalidPacketIds trait

---
+++ src/Packetery/Core/Api/Soap/ResponseTrait/InvalidPacketIds.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Class InvalidPacketIds
+ *
+ * @package Packetery\Api\Soap\ResponseTrait
+ */
+
+declare( strict_types=1 );
+
+
+namespace Packetery\Core\Api\Soap\ResponseTrait;
+
+/**
+ * Class InvalidPacketIds
+ *
+ * @package Packetery\Api\Soap\ResponseTrait
+ */
+trait InvalidPacketIds {
+
+	/**
+	 * Invalid packet IDs
+	 *
+	 * @var string[]
+	 */
+	private $invalidPacketIds = [];
+
+	/**
+	 * Sets invalid packet IDs.
+	 *
+	 * @param array $invalidPacketIds Invalid packet IDs.
+	 *
+	 * @return void
+	 */
+	public function setInvalidPacketIds( array $invalidPacketIds ): void {
+		$this->invalidPacketIds = $invalidPacketIds;
+	}
+
+	/**
+	 * Tells if response has invalid packet ID.
+	 *
+	 * @param string $packetId Packet ID.
+	 *
+	 * @return bool|null
+	 */
+	public function hasInvalidPacketId( string $packetId ): ?bool {
+		if ( in_array( $packetId, $this->invalidPacketIds, true ) ) {
+			return true;
+		}
+
+		return null;
+	}
+}
