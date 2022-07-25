a847f9485a8eeba4d20f8f7e6c0e644358811e72
Filip Jiskra <filip.jiskra@packeta.com>
2022-07-25 12:33:29 +0200

    PES-521: shipping product limitation - rebase fix

--- src/Packetery/Module/Plugin.php
+++ src/Packetery/Module/Plugin.php
@@ -715,7 +715,7 @@ class Plugin {
 			$this->enqueueScript( 'packetery-admin-country-carrier', 'public/admin-country-carrier.js', true, [ 'jquery' ] );
 		}
 
-		$isProductDetailPage = 'post.php' === $pagenow && 'product' === $typenow;
+		$isProductDetailPage = $this->contextResolver->isProductDetailPage();
 
 		if ( $isOrderGridPage || $isOrderDetailPage || $isProductDetailPage || in_array( $page, [ Options\Page::SLUG, Carrier\OptionsPage::SLUG, Log\Page::SLUG, Order\labelPrint::MENU_SLUG ], true ) ) {
 			$this->enqueueStyle( 'packetery-admin-styles', 'public/admin.css' );
