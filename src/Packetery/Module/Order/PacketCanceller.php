84d27f555694e1d1b4e538195b51f259611a509e
jan-potuznik-z <79142524+jan-potuznik-z@users.noreply.github.com>
2022-08-04 16:17:10 +0200

    PES-922: fixed NPEs and typo in PacketCanceller (#268)
    * PES-922: fixed NPEs and typo in PacketCanceller
    
    * PES-922: getOrderId method revived

--- src/Packetery/Module/Order/PacketCanceller.php
+++ src/Packetery/Module/Order/PacketCanceller.php
@@ -123,12 +123,10 @@ class PacketCanceller {
 		}
 
 		$redirectTo = $this->request->getQuery( self::PARAM_REDIRECT_TO );
-		$order      = $this->orderRepository->getById( $this->getOrderId() );
-
-		if ( 1 !== wp_verify_nonce( $this->request->getQuery( Plugin::PARAM_NONCE ), self::createNonceAction( self::ACTION_CANCEL_PACKET, $order->getNumber() ) ) ) {
-			$this->messageManager->flash_message( __( 'Link has expired. Please try again.', 'packeta' ), MessageManager::TYPE_ERROR );
-			$this->redirectTo( $redirectTo, $order );
-			return;
+		$order      = null;
+		$orderId    = $this->getOrderId();
+		if ( null !== $orderId ) {
+			$order = $this->orderRepository->getById( $orderId );
 		}
 
 		if ( null === $order ) {
@@ -145,6 +143,12 @@ class PacketCanceller {
 			$this->logger->add( $record );
 
 			$this->messageManager->flash_message( __( 'Order not found', 'packeta' ), MessageManager::TYPE_ERROR );
+			$this->redirectTo( $redirectTo, $order );
+			return;
+		}
+
+		if ( 1 !== wp_verify_nonce( $this->request->getQuery( Plugin::PARAM_NONCE ), self::createNonceAction( self::ACTION_CANCEL_PACKET, $order->getNumber() ) ) ) {
+			$this->messageManager->flash_message( __( 'Link has expired. Please try again.', 'packeta' ), MessageManager::TYPE_ERROR );
 			$this->redirectTo( $redirectTo, $order );
 			return;
 		}
@@ -175,7 +179,7 @@ class PacketCanceller {
 			}
 		}
 
-		if ( self::REDIRECT_TO_ORDER_DETAIL === $redirectTo ) {
+		if ( self::REDIRECT_TO_ORDER_DETAIL === $redirectTo && null !== $order ) {
 			$packetCancelLink = add_query_arg(
 				[
 					'post_type' => 'shop_order',
@@ -284,13 +288,13 @@ class PacketCanceller {
 			return false;
 		}
 
-		$revertSubmition = ! $result->hasFault();
+		$revertSubmission = ! $result->hasFault();
 
 		if ( $result->hasCancelNotAllowedFault() && $this->optionsProvider->isPacketCancellationForced() ) {
-			$revertSubmition = true;
+			$revertSubmission = true;
 		}
 
-		return $revertSubmition;
+		return $revertSubmission;
 	}
 
 	/**
