258b7e421f03cf832b6e45c0cc82fd053c38be15
Mrakor <20678997+Mrakor@users.noreply.github.com>
2022-10-13 14:09:04 +0200

    PES-871 CR improvement: make Rounder more versatile and universal

--- src/Packetery/Core/Rounder.php
+++ src/Packetery/Core/Rounder.php
@@ -13,12 +13,37 @@ namespace Packetery\Core;
 /**
  * Class Rounder
  *
+ * Class for rounding NON-NEGATIVE numbers
+ *
  * @package Packetery
  */
 class Rounder {
-	public const ROUND_UP   = 1;
-	public const ROUND_DOWN = -1;
-	public const DONT_ROUND = 0;
+	public const ROUND_UP       = 1;
+	public const ROUND_DOWN     = -1;
+	public const DONT_ROUND     = 0;
+	public const ROUNDING_TYPES = [ self::ROUND_UP, self::ROUND_DOWN, self::DONT_ROUND ];
+
+	/**
+	 * Returns ceil of amount. Should solve float precision problems along the way.
+	 *
+	 * @param float $amount Amount to ceil.
+	 *
+	 * @return float
+	 */
+	public static function ceil( float $amount ): float {
+		return ceil( (float) ( (string) $amount ) );
+	}
+
+	/**
+	 * Returns floor of amount. Should solve float precision problems along the way.
+	 *
+	 * @param float $amount Amount to floor.
+	 *
+	 * @return float
+	 */
+	public static function floor( float $amount ): float {
+		return floor( (float) ( (string) $amount ) );
+	}
 
 	/**
 	 * Returns rounded amount.
@@ -28,16 +53,21 @@ class Rounder {
 	 * @param int   $precision Precision (number of digits after decimal point).
 	 *
 	 * @return float
+	 * @throws \InvalidArgumentException InvalidArgumentException.
 	 */
-	private static function customRound( float $amount, int $roundingType, int $precision ): float {
+	public static function round( float $amount, int $roundingType, int $precision ): float {
+		if ( ( $amount < 0 ) || ( $precision < 0 ) || ! in_array( $roundingType, self::ROUNDING_TYPES, true ) ) {
+			throw new \InvalidArgumentException( 'Amount and precision should be non-negative numbers, roundingType should be one of [-1, 0, 1].' );
+		}
+
 		$amount *= 10 ** $precision;
 
 		if ( self::ROUND_DOWN === $roundingType ) {
-			$amount = floor( (float) ( (string) $amount ) ); // conversion float to string to float should fix float precision problems.
+			$amount = self::floor( $amount );
 		}
 
 		if ( self::ROUND_UP === $roundingType ) {
-			$amount = ceil( (float) ( (string) $amount ) );
+			$amount = self::ceil( $amount );
 		}
 
 		$amount /= ( 10 ** $precision );
@@ -46,25 +76,55 @@ class Rounder {
 	}
 
 	/**
-	 * Returns rounded amount to multiple of 5.
+	 * Rounds amount up according to precision.
 	 *
-	 * @param float $amount Amount.
-	 * @param int   $roundingType Type of rounding.
+	 * @param float $amount Amount to round.
+	 * @param int   $precision Precision (number of digits after decimal point).
 	 *
 	 * @return float
 	 */
-	private static function roundToMultipleOfFive( float $amount, int $roundingType ): float {
-		$amount /= 5;
+	public static function roundUp( float $amount, int $precision = 0 ): float {
+		return self::round( $amount, self::ROUND_UP, $precision );
+	}
 
-		if ( self::ROUND_UP === $roundingType ) {
-			$amount = ceil( (float) ( (string) $amount ) );
+	/**
+	 * Rounds amount up according to precision.
+	 *
+	 * @param float $amount Amount to round.
+	 * @param int   $precision Precision (number of digits after decimal point).
+	 *
+	 * @return float
+	 */
+	public static function roundDown( float $amount, int $precision = 0 ): float {
+		return self::round( $amount, self::ROUND_DOWN, $precision );
+	}
+
+	/**
+	 * Returns rounded amount to multiple of $number.
+	 *
+	 * @param float $amount Amount.
+	 * @param int   $roundingType Type of rounding.
+	 * @param int   $number Round to multiple of this.
+	 *
+	 * @return float
+	 * @throws \InvalidArgumentException InvalidArgumentException.
+	 */
+	public static function roundToMultipleOfNumber( float $amount, int $roundingType, int $number ): float {
+		if ( ( $amount < 0 ) || ( $number < 1 ) || ! in_array( $roundingType, self::ROUNDING_TYPES, true ) ) {
+			throw new \InvalidArgumentException( 'Amount should be non-negative number, number should be positive number roundingType should be one of [-1, 0, 1].' );
 		}
+
+		$amount /= $number;
 
 		if ( self::ROUND_DOWN === $roundingType ) {
-			$amount = floor( (float) ( (string) $amount ) );
+			$amount = self::floor( $amount );
 		}
 
-		return 5 * $amount;
+		if ( self::ROUND_UP === $roundingType ) {
+			$amount = self::ceil( $amount );
+		}
+
+		return $number * $amount;
 	}
 
 	/**
@@ -75,16 +135,22 @@ class Rounder {
 	 * @param int    $roundingType Type of rounding.
 	 *
 	 * @return float
+	 * @throws \InvalidArgumentException InvalidArgumentException.
 	 */
 	public static function roundByCurrency( float $amount, string $currencyCode, int $roundingType ): float {
+		if ( ( $amount < 0 ) || ! in_array( $roundingType, self::ROUNDING_TYPES, true ) ) {
+			throw new \InvalidArgumentException( 'Amount should be non-negative number, roundingType should be one of [-1, 0, 1].' );
+		}
+
 		if ( 'CZK' === $currencyCode ) {
-			return self::customRound( $amount, $roundingType, 0 );
+			return self::round( $amount, $roundingType, 0 );
 		}
 
 		if ( 'HUF' === $currencyCode ) {
-			return self::roundToMultipleOfFive( $amount, $roundingType );
+			return self::roundToMultipleOfNumber( $amount, $roundingType, 5 );
 		}
 
-		return self::customRound( $amount, $roundingType, 2 );
+		return self::round( $amount, $roundingType, 2 );
 	}
+
 }
