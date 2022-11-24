b4fbe0dd303b4eb0e4ebf904495c00515bfbe207
jan-potuznik-z <79142524+jan-potuznik-z@users.noreply.github.com>
2022-11-24 08:27:26 +0100

    PES-1077: Show all payment methods in plugin settings (#314)
    * PES-1077: Show all payment methods in plugin settings
    
    * PES-1077: CR: show all enabled payment methods in plugin settings

--- src/Packetery/Module/Options/Page.php
+++ src/Packetery/Module/Options/Page.php
@@ -468,12 +468,7 @@ class Page {
 			}
 		}
 
-		/**
-		 * Applies woocommerce_available_payment_gateways filters.
-		 *
-		 * @since 1.0.5
-		 */
-		return array_filter( (array) apply_filters( 'woocommerce_available_payment_gateways', $availableGateways ), [ $this, 'filterValidGatewayClass' ] );
+		return array_filter( $availableGateways, [ $this, 'filterValidGatewayClass' ] );
 	}
 
 	/**
