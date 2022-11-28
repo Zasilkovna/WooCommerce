d493a7075471a0b7df14421a8bd98ccaf0f33ef2
Marek Mozola <113614605+MMozola@users.noreply.github.com>
2022-11-28 11:43:01 +0100

    PES-1100_shipping_price_filter (#322)
    * PES-1100: packeta shipping price filter
    
    * PES-1100: code optimize
    
    * PES-1100: code optimize
    
    Co-authored-by: Marek Mozola <marek@mozola.net>

--- src/Packetery/Module/Checkout.php
+++ src/Packetery/Module/Checkout.php
@@ -960,6 +960,13 @@ class Checkout {
 	 * @return array
 	 */
 	private function createShippingRate( string $name, string $optionId, ?float $cost ): array {
+		/**
+		 * Filter shipping rate cost in checkout
+		 *
+		 * @since 1.4.1
+		 */
+		$cost = apply_filters( 'packeta_shipping_price', $cost );
+
 		return [
 			'label'    => $name,
 			'id'       => $optionId,
