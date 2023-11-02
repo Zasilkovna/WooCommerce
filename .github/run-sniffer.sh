0de4ddd48cae2cd4aa4a26647b13fd5d3de32c58
Filip Jiskra <filip.jiskra@packeta.com>
2023-11-02 16:40:16 +0100

    PES: sniffer pipeline optimalization

--- .github/run-sniffer.sh
+++ .github/run-sniffer.sh
@@ -9,6 +9,11 @@ php -v | head -1
 echo "ℹ︎ Composer version:"
 composer -V
 
+echo "➤︎ Clean up:"
+rm ./composer.json ./composer.lock ./composer-deps.json ./composer-deps.lock
+echo "{}" > composer.json
+composer config --no-plugins allow-plugins.dealerdirect/phpcodesniffer-composer-installer true
+
 echo "➤ Installing woocommerce/woocommerce-sniffs:"
 composer -n -q require woocommerce/woocommerce-sniffs:0.1.3
 
