d03d3355fc4368c528202e4a0c99228c3fad1618
Jan Potužník <jan.potuznik@gmail.com>
2023-12-08 15:37:12 +0100

    PES-1815: invoice date validation added

--- public/live-form-validation-extension.js
+++ public/live-form-validation-extension.js
@@ -5,3 +5,7 @@ Nette.validators['PacketeryModuleFormValidators_greaterThan'] = (elem, args, val
 Nette.validators['PacketeryModuleFormValidators_dateIsLater'] = (elem, args, val) => {
     return new Date(val).getTime() > new Date(args).getTime();
 };
+
+Nette.validators['PacketeryModuleFormValidators_dateIsInMysqlFormat'] = (elem, args, val) => {
+    return !isNaN( new Date( val ).getTime() );
+};
