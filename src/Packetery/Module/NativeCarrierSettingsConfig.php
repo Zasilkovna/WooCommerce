a817b77a4815ae219ccca6dde83563ce3518df77
David Belka <david.belka@yahoo.co.uk>
2024-03-14 09:45:35 +0100

    PES-1991: Added a new config for native packeta carrier settings.

---
+++ src/Packetery/Module/NativeCarrierSettingsConfig.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Class NativeCarrierSettingsConfig
+ *
+ * @package Packetery
+ */
+
+namespace Packetery\Module;
+
+/**
+ * Class NativeCarrierSettingsConfig
+ *
+ * @property bool $active
+ * @package Packetery
+ */
+class NativeCarrierSettingsConfig {
+
+	/**
+	 * NativeCarrierSettingsConfig constructor.
+	 *
+	 * @param bool $active Sample.
+	 */
+	public function __construct( bool $active ) {
+		$this->active = $active;
+	}
+
+	/**
+	 * Tells whether the native Packeta carrier settings is active.
+	 *
+	 * @return bool
+	 */
+	public function isSettingsActive(): bool {
+		return $this->active;
+	}
+
+}
