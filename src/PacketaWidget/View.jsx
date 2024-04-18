7affbff01ac5c7770575b1d7cdf8eccfdbaa177f
packeta <57401091+packeta@users.noreply.github.com>
2024-04-18 16:45:31 +0200

    PES-2009: checkout Packeta widget opening

--- src/PacketaWidget/View.jsx
+++ src/PacketaWidget/View.jsx
@@ -1,8 +1,9 @@
+import {__} from "@wordpress/i18n";
 import { getSetting } from '@woocommerce/settings';
 
 export const View = ({cart}) => {
     const {shippingRates} = cart;
-    const {carrierConfig: packetaWidgetCarrierConfig} = getSetting('packeta-widget_data');
+    const {carrierConfig: packetaWidgetCarrierConfig, packeteryApiKey} = getSetting('packeta-widget_data');
 
     console.log('initial', shippingRates);
     if (!shippingRates || shippingRates.length === 0) {
@@ -28,10 +29,18 @@ export const View = ({cart}) => {
         return null;
     }
 
+    const onWidgetButtonClicked = () => {
+        // TODO: properly load Packeta object
+        Packeta.Widget.pick( packeteryApiKey, ( result ) => {
+            console.log(result);
+        });
+    };
+
     console.log('rendered');
     return <tr className="packetery-widget-button-table-row">
         <th>
             <img className="packetery-widget-button-logo" src="/wp-content/plugins/packeta/public/packeta-symbol.png" alt="packeta" width="100" />
+            <button onClick={onWidgetButtonClicked}>{__('Select pickup point')}</button>
         </th>
     </tr>
 }
