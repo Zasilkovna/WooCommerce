536ac1ef1211fab7ce39347dd9440862593bee27
David Belka <54403115+DBelka@users.noreply.github.com>
2024-05-10 10:45:32 +0200

    PES-2043, PES-2050: New carrier option switch in settings (#523)
    * PES-2043: Added a new 'Advanced' tab in settings for WC carrier settings switch and made some changes to the styling across the settings tabs
    
    * PES-2050: rebase fix
    
    ---------
    
    Co-authored-by: Jan Potužník <jan.potuznik@gmail.com>

--- src/Packetery/Module/Carrier/OptionsPage.php
+++ src/Packetery/Module/Carrier/OptionsPage.php
@@ -17,6 +17,7 @@ use Packetery\Module\FormFactory;
 use Packetery\Module\FormValidators;
 use Packetery\Module\MessageManager;
 use Packetery\Module\Options\FeatureFlagManager;
+use Packetery\Module\Options\Provider;
 use Packetery\Module\PaymentGatewayHelper;
 use Packetery\Nette\Forms\Container;
 use Packetery\Nette\Forms\Form;
@@ -103,9 +104,9 @@ class OptionsPage {
 	/**
 	 * WC Native carrier settings config.
 	 *
-	 * @var WcSettingsConfig
+	 * @var Provider
 	 */
-	private $wcSettingsConfig;
+	private $optionsProvider;
 
 	/**
 	 * OptionsPage constructor.
@@ -119,7 +120,7 @@ class OptionsPage {
 	 * @param PacketaPickupPointsConfig $pickupPointsConfig Internal pickup points config.
 	 * @param FeatureFlagManager        $featureFlag        Feature flag.
 	 * @param CarDeliveryConfig         $carDeliveryConfig  Car delivery config.
-	 * @param WcSettingsConfig          $wcSettingsConfig   WC Native carrier settings config.
+	 * @param Provider                  $optionsProvider    Options provider.
 	 */
 	public function __construct(
 		Engine $latteEngine,
@@ -131,7 +132,7 @@ class OptionsPage {
 		PacketaPickupPointsConfig $pickupPointsConfig,
 		FeatureFlagManager $featureFlag,
 		CarDeliveryConfig $carDeliveryConfig,
-		WcSettingsConfig $wcSettingsConfig
+		Provider $optionsProvider
 	) {
 		$this->latteEngine        = $latteEngine;
 		$this->carrierRepository  = $carrierRepository;
@@ -142,7 +143,7 @@ class OptionsPage {
 		$this->pickupPointsConfig = $pickupPointsConfig;
 		$this->featureFlag        = $featureFlag;
 		$this->carDeliveryConfig  = $carDeliveryConfig;
-		$this->wcSettingsConfig   = $wcSettingsConfig;
+		$this->optionsProvider    = $optionsProvider;
 	}
 
 	/**
@@ -175,7 +176,7 @@ class OptionsPage {
 
 		$form = $this->formFactory->create( $optionId );
 
-		if ( false === $this->wcSettingsConfig->isActive() ) {
+		if ( false === $this->optionsProvider->isWcCarrierConfigEnabled() ) {
 			$form->addCheckbox(
 				self::FORM_FIELD_ACTIVE,
 				__( 'Active carrier', 'packeta' ) . ':'
@@ -393,7 +394,7 @@ class OptionsPage {
 			$options['vendor_groups'] = $newVendors;
 		}
 
-		if ( $this->wcSettingsConfig->isActive() ) {
+		if ( $this->optionsProvider->isWcCarrierConfigEnabled() ) {
 			$persistedOptions                   = Options::createByCarrierId( $options['id'] );
 			$options[ self::FORM_FIELD_ACTIVE ] = $persistedOptions->isActive();
 		}
