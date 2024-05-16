87cec9ee9496f425cff2832ab82a479045b79c6e
jan-potuznik-z <79142524+jan-potuznik-z@users.noreply.github.com>
2024-05-16 15:34:23 +0200

    PES-2043, PES-2050: New carrier option switch in settings (#552)
    * PES-2043: Added a new 'Advanced' tab in settings for WC carrier settings switch and made some changes to the styling across the settings tabs
    
    * PES-2050: rebase fix
    
    ---------
    
    Co-authored-by: David Belka <david.belka@yahoo.co.uk>

--- src/Packetery/Module/ShippingMethod.php
+++ src/Packetery/Module/ShippingMethod.php
@@ -10,7 +10,6 @@ declare( strict_types=1 );
 namespace Packetery\Module;
 
 use Packetery\Latte\Engine;
-use Packetery\Module\Carrier\WcSettingsConfig;
 use Packetery\Nette\DI\Container;
 
 /**
@@ -57,11 +56,11 @@ class ShippingMethod extends \WC_Shipping_Method {
 	private $latteEngine;
 
 	/**
-	 * Are we using carrier settings native for WooCommerce?
+	 * Options provider.
 	 *
-	 * @var WcSettingsConfig
+	 * @var Options\Provider
 	 */
-	private $wcCarrierSettingsConfig;
+	private $optionsProvider;
 
 	/**
 	 * Constructor for Packeta shipping class
@@ -71,8 +70,8 @@ class ShippingMethod extends \WC_Shipping_Method {
 	public function __construct( int $instance_id = 0 ) {
 		parent::__construct();
 
-		$this->container               = CompatibilityBridge::getContainer();
-		$this->wcCarrierSettingsConfig = $this->container->getByType( WcSettingsConfig::class );
+		$this->container       = CompatibilityBridge::getContainer();
+		$this->optionsProvider = $this->container->getByType( Options\Provider::class );
 
 		$this->id           = self::PACKETERY_METHOD_ID;
 		$this->instance_id  = absint( $instance_id );
@@ -83,7 +82,7 @@ class ShippingMethod extends \WC_Shipping_Method {
 			'shipping-zones',
 		];
 
-		if ( $this->wcCarrierSettingsConfig->isActive() ) {
+		if ( $this->optionsProvider->isWcCarrierConfigEnabled() ) {
 			$this->method_description = __( 'Allows to choose one of Packeta delivery methods', 'packeta' );
 			$this->supports[]         = 'instance-settings';
 			$this->supports[]         = 'instance-settings-modal';
@@ -99,7 +98,7 @@ class ShippingMethod extends \WC_Shipping_Method {
 	 * Init user set variables. Derived from WC_Shipping_Flat_Rate.
 	 */
 	public function init(): void {
-		if ( ! $this->wcCarrierSettingsConfig->isActive() ) {
+		if ( ! $this->optionsProvider->isWcCarrierConfigEnabled() ) {
 			add_action(
 				'woocommerce_update_options_shipping_' . $this->id,
 				[
@@ -121,7 +120,7 @@ class ShippingMethod extends \WC_Shipping_Method {
 	 * @return string
 	 */
 	public function get_admin_options_html(): string {
-		if ( ! $this->wcCarrierSettingsConfig->isActive() ) {
+		if ( ! $this->optionsProvider->isWcCarrierConfigEnabled() ) {
 			return '';
 		}
 
@@ -142,7 +141,7 @@ class ShippingMethod extends \WC_Shipping_Method {
 	public function calculate_shipping( $package = [] ): void {
 		$allowedCarrierNames = null;
 
-		if ( $this->wcCarrierSettingsConfig->isActive() ) {
+		if ( $this->optionsProvider->isWcCarrierConfigEnabled() ) {
 			$allowedCarrierNames = [];
 			$zone                = \WC_Shipping_Zones::get_zone_matching_package( $package );
 			$shippingMethods     = $zone->get_shipping_methods( true );
