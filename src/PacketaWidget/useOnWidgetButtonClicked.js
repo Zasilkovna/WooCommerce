ca669e9abdfd3a3d079ea37e6b7024ff732dc8b2
jan-potuznik-z <79142524+jan-potuznik-z@users.noreply.github.com>
2024-05-16 15:17:04 +0200

    PES-2010: blocks checkout support (#550)
    * PES-1854: second update_order_meta hook registered
    
    * WC Blocks WIP
    
    * PES-1854 IntegrationInterface not working.
    
    * PES-1854: script integration
    
    * PES-2047 WC Blocks work in progress
    
    * PES-2009: bugfixes
    
    * PES-2009: checkout Packeta widget opening (#525)
    
    * PES-2009: checkout Packeta widget opening
    
    * PES-2009: passing options to widget
    
    ---------
    
    Co-authored-by: packeta <57401091+packeta@users.noreply.github.com>
    Co-authored-by: Jan Potužník <jan.potuznik@gmail.com>
    
    * PES-2009: PHP 7.2 compatibility
    
    * PES-2009: conditional checkout.js loading
    
    * PES-2009: translations, code style
    
    * PES-2009: widgetAutoOpen support
    
    * PES-2047 Show information about pickup point under the widget button.
    
    * PES-2009: better widget loading
    
    * PES-2009: saving chosen pickup point data
    
    * PES-2009: packetery-renderer attribute removed
    
    * PES-2009: css update
    
    * PES-2010: ValidatedTextInput WIP
    
    * PES-2047 View component refactored.
    
    * PES-2047 Callback fixed.
    
    * PES-2010: PP validation
    
    * PES-2059: dynamic loading of some settings, partial code style update
    
    * PES-2047 Finalization refactor.
    
    * PES-2059: minor changes, code style update
    
    * PES-2010: css for backend
    
    * PES-2010: minor changes, code style update
    
    * PES-2010: namespace update, plugin version utilised
    
    * PES-2010: proper handling of HD order
    
    ---------
    
    Co-authored-by: Stanislav Kocanda <stanislav.kocanda@packeta.com>
    Co-authored-by: FJiskra <filip.jiskra@gmail.com>
    Co-authored-by: packeta <57401091+packeta@users.noreply.github.com>

---
+++ src/PacketaWidget/useOnWidgetButtonClicked.js
@@ -0,0 +1,109 @@
+/**
+ * Widget button callback.
+ *
+ * @package Packetery
+ */
+
+import { useCallback, useState } from 'react';
+
+export const useOnWidgetButtonClicked = (
+	packetaShippingRate,
+	settings,
+	dynamicSettings
+) => {
+	const {
+		carrierConfig,
+		country,
+		language,
+		packeteryApiKey,
+		appIdentity,
+		nonce,
+		saveSelectedPickupPointUrl,
+		pickupPointAttrs,
+	} = settings;
+
+	const [ viewState, setViewState ] = useState( null );
+
+	const onWidgetButtonClicked = useCallback( () => {
+		const rateId = packetaShippingRate.rate_id.split( ':' ).pop();
+
+		let weight = 0.0;
+		if ( dynamicSettings && dynamicSettings.weight ) {
+			weight = dynamicSettings.weight;
+		}
+		let widgetOptions = { country, language, appIdentity, weight };
+
+		if ( carrierConfig[ rateId ].carriers ) {
+			widgetOptions.carriers = carrierConfig[ rateId ].carriers;
+		}
+		if ( carrierConfig[ rateId ].vendors ) {
+			widgetOptions.vendors = carrierConfig[ rateId ].vendors;
+		}
+		if ( dynamicSettings && dynamicSettings.isAgeVerificationRequired ) {
+			widgetOptions.livePickupPoint = true; // Pickup points with real person only.
+		}
+
+		const fillRateAttrValues = function ( carrierRateId, data, source ) {
+			for ( let attrKey in data ) {
+				if ( ! data.hasOwnProperty( attrKey ) ) {
+					continue;
+				}
+
+				const { name, widgetResultField, isWidgetResultField } =
+					data[ attrKey ];
+
+				if ( false === isWidgetResultField ) {
+					continue;
+				}
+
+				let widgetField = widgetResultField || attrKey;
+				let addressFieldValue = source[ widgetField ];
+
+				rateAttrValues[ carrierRateId ] =
+					rateAttrValues[ carrierRateId ] || {};
+				rateAttrValues[ carrierRateId ][ name ] = addressFieldValue;
+			}
+		};
+
+		// Storage to store settings of all Packeta shipping methods displayed at checkout.
+		let rateAttrValues = {};
+
+		Packeta.Widget.pick(
+			packeteryApiKey,
+			( pickupPoint ) => {
+				if ( ! pickupPoint ) {
+					return;
+				}
+
+				setViewState( { pickupPoint } );
+
+				fillRateAttrValues( rateId, pickupPointAttrs, pickupPoint );
+				let pickupPointDataToSave = rateAttrValues[ rateId ];
+				pickupPointDataToSave.packetery_rate_id = rateId;
+
+				fetch( saveSelectedPickupPointUrl, {
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/x-www-form-urlencoded',
+						'X-WP-Nonce': nonce,
+					},
+					body: new URLSearchParams( pickupPointDataToSave ),
+				} )
+					.then( ( response ) => {
+						if ( ! response.ok ) {
+							throw new Error( 'HTTP error ' + response.status );
+						}
+					} )
+					.catch( ( error ) => {
+						console.error(
+							'Failed to save pickup point data:',
+							error
+						);
+					} );
+			},
+			widgetOptions
+		);
+	}, [ packetaShippingRate, dynamicSettings ] );
+
+	return [ onWidgetButtonClicked, viewState ];
+};
---
+++ src/Packetery/Module/Blocks/WidgetIntegration.php
@@ -0,0 +1,119 @@
+<?php
+/**
+ * WidgetIntegration.
+ *
+ * @package Packetery
+ */
+
+namespace Packetery\Module\Blocks;
+
+use Automattic\WooCommerce\Blocks\Integrations\IntegrationInterface;
+use Packetery\Module\Plugin;
+
+if ( file_exists( WP_PLUGIN_DIR . '/woocommerce/src/Blocks/Integrations/IntegrationInterface.php' ) ) {
+	require_once WP_PLUGIN_DIR . '/woocommerce/src/Blocks/Integrations/IntegrationInterface.php';
+}
+
+if ( file_exists( WP_PLUGIN_DIR . '/woocommerce/packages/woocommerce-blocks/src/Integrations/IntegrationInterface.php' ) ) {
+	require_once WP_PLUGIN_DIR . '/woocommerce/packages/woocommerce-blocks/src/Integrations/IntegrationInterface.php';
+}
+
+/**
+ * WidgetIntegration.
+ *
+ * @package Packetery
+ */
+class WidgetIntegration implements IntegrationInterface {
+	const INTEGRATION_NAME = 'packeta-widget';
+
+	/**
+	 * Settings.
+	 *
+	 * @var array
+	 */
+	public $settings;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param array $settings Settings.
+	 */
+	public function __construct( array $settings ) {
+		$this->settings = $settings;
+	}
+
+	/**
+	 * Gets name.
+	 *
+	 * @return string
+	 */
+	public function get_name(): string {
+		return self::INTEGRATION_NAME;
+	}
+
+	/**
+	 * Initializes.
+	 *
+	 * @return void
+	 */
+	public function initialize(): void {
+		$scriptPath      = '/packeta/public/js/index.js';
+		$scriptAssetPath = PACKETERY_PLUGIN_DIR . '/public/js/index.asset.php';
+		$scriptAsset     = file_exists( $scriptAssetPath )
+			? require $scriptAssetPath
+			: [
+				'dependencies' => [],
+				'version'      => $this->get_file_version( $scriptPath ),
+			];
+
+		wp_register_script(
+			self::INTEGRATION_NAME,
+			plugins_url( $scriptPath ),
+			$scriptAsset['dependencies'],
+			$scriptAsset['version'],
+			true
+		);
+	}
+
+	/**
+	 * Gets script handles.
+	 *
+	 * @return string[]
+	 */
+	public function get_script_handles() {
+		return [ self::INTEGRATION_NAME ];
+	}
+
+	/**
+	 * Gets editor script handles.
+	 *
+	 * @return string[]
+	 */
+	public function get_editor_script_handles(): array {
+		return [ self::INTEGRATION_NAME ];
+	}
+
+	/**
+	 * Gets script data.
+	 *
+	 * @return array
+	 */
+	public function get_script_data(): array {
+		return $this->settings;
+	}
+
+	/**
+	 * Gets file version.
+	 *
+	 * @param string $filePath File path.
+	 *
+	 * @return bool|int|string
+	 */
+	protected function get_file_version( string $filePath ) {
+		if ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG && file_exists( $filePath ) ) {
+			return filemtime( $filePath );
+		}
+
+		return Plugin::VERSION;
+	}
+}
