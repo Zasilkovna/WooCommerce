82df4f914b51044499eb398fafbd249dbf5b545a
Jan Potužník <jan.potuznik@gmail.com>
2024-05-20 14:07:07 +0200

    PES-2082: compiled assets removed from gitignore

---
+++ public/js/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wc-blocks-checkout', 'wc-blocks-components', 'wc-settings', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '064ea2866c2913f88b4c');
