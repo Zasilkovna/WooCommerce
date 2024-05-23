39c6ae67b51b15ac5287e27399ca13f9e47fa46f
David Belka <david.belka@yahoo.co.uk>
2024-05-23 09:16:54 +0200

    PES-2092: Assigned an error message to a variable

--- tests/Core/Api/InvalidRequestExceptionTest.php
+++ tests/Core/Api/InvalidRequestExceptionTest.php
@@ -10,10 +10,11 @@ use PHPUnit\Framework\TestCase;
 class InvalidRequestExceptionTest extends TestCase {
 
 	public function testGetMessages(): void {
-		$invalidException = new InvalidRequestException( 'Error message', ['some error message'] );
+		$message = 'some error message';
+		$invalidException = new InvalidRequestException( 'Error message', [$message] );
 
 		self::assertIsArray( $invalidException->getMessages() );
-		self::assertContains( 'some error message', $invalidException->getMessages() );
+		self::assertContains( $message, $invalidException->getMessages() );
 	}
 
 }
