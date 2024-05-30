67df0af138fd15476066999d438b197128bb4f17
jan-potuznik-z <79142524+jan-potuznik-z@users.noreply.github.com>
2024-05-30 10:44:06 +0200

    PES-2079: Added Packet logo to edit.jsx (#562)
    Co-authored-by: Jaroslav Malý <jaroslav.maly@packeta.com>

--- public/js/index.asset.php
+++ public/js/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-checkout', 'wc-blocks-components', 'wc-settings', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'bbae264722ca9794382f');
+<?php return array('dependencies' => array('react', 'wc-blocks-checkout', 'wc-blocks-components', 'wc-settings', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '3b6cdb0b20e37392ee3a');
