57dcd0ca2f236dbb2c8fe2b56940c1d755473053
Jan Potužník <jan.potuznik@gmail.com>
2024-05-31 09:54:27 +0200

    PES-2109: country change detection update

--- public/js/index.asset.php
+++ public/js/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-checkout', 'wc-blocks-components', 'wc-settings', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '3b6cdb0b20e37392ee3a');
+<?php return array('dependencies' => array('react', 'wc-blocks-checkout', 'wc-blocks-components', 'wc-settings', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'af30ad0b74406908ad0a');
