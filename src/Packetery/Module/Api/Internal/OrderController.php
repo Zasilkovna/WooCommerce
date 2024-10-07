9f1d531021d50fd2798e4a94b9c4cd4bf138f06f
Adam Karpita <adam.karpita@trayto.com>
2024-10-07 13:39:23 +0200

    fixup! PES-2238: Added modal popup and basics for extending a shipment stored until date

--- src/Packetery/Module/Api/Internal/OrderController.php
+++ src/Packetery/Module/Api/Internal/OrderController.php
@@ -88,7 +88,6 @@ final class OrderController extends WP_REST_Controller {
 	 */
 	private $packetSetStoredUntil;
 
-
 	/**
 	 * Controller constructor.
 	 *
@@ -244,7 +243,7 @@ final class OrderController extends WP_REST_Controller {
 	 *
 	 * @return WP_REST_Response|WP_Error
 	 */
-	public function saveStoredUntil( WP_REST_Request $request ): WP_Error|WP_REST_Response {
+	public function saveStoredUntil( WP_REST_Request $request ) {
 		$data        = [];
 		$parameters  = $request->get_body_params();
 		$orderId     = (int) $parameters['orderId'];
