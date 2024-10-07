c40fd570c8e377fa94a4a3bd11ebc11e74a146f9
Adam Karpita <adam.karpita@trayto.com>
2024-10-07 13:31:03 +0200

    fixup! PES-2277: Add sending stored until date to packetery api and synchronizing with packet status

--- src/Packetery/Core/Api/Soap/Client.php
+++ src/Packetery/Core/Api/Soap/Client.php
@@ -318,7 +318,7 @@ class Client {
 	}
 
 	/**
-	 * Requests for sender return routing strings.
+	 * Requests for set new stored until date.
 	 *
 	 * @param Request\PacketSetStoredUntil $request Request.
 	 *
