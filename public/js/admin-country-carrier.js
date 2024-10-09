c64512c4c5b8dc9e6c9bdb93fca6b0d52a932053
FJiskra <filip.jiskra@packeta.com>
2024-10-09 11:21:03 +0200

    PES-2262: editor config update: tabs used - js

--- public/js/admin-country-carrier.js
+++ public/js/admin-country-carrier.js
@@ -1,16 +1,16 @@
 (function ($) {
 
-    $(function () {
-        // are we on the right page?
-        if ($('.packetery-carrier-options-page').length === 0) {
-            return;
-        }
+	$(function () {
+		// are we on the right page?
+		if ($('.packetery-carrier-options-page').length === 0) {
+			return;
+		}
 
-        new PacketeryMultiplier('.js-weight-rules');
-        new PacketeryMultiplier('.js-product-value-rules');
-        new PacketeryMultiplier('.js-surcharge-rules');
+		new PacketeryMultiplier('.js-weight-rules');
+		new PacketeryMultiplier('.js-product-value-rules');
+		new PacketeryMultiplier('.js-surcharge-rules');
 
-        $( '[data-packetery-select2]' ).select2();
-    });
+		$( '[data-packetery-select2]' ).select2();
+	});
 
 })(jQuery);
