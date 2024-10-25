ad5f29b3b46b2e49d2e34f9cefef2825b7a74d3e
Adam Karpita <adam.karpita@packeta.com>
2024-10-25 11:31:18 +0200

    fixup! PES-2277: Refactor test for PacketSetSoterdUntil class

--- tests/Module/Order/PacketSetStoredUntilTest.php
+++ tests/Module/Order/PacketSetStoredUntilTest.php
@@ -7,11 +7,7 @@ namespace Tests\Module\Order;
 use Packetery\Core\Api\Soap\Client;
 use Packetery\Core\Log\ILogger;
 use Packetery\Core\Api\Soap\Response\PacketSetStoredUntil as ResponsePacketSetStoredUntil;
-use Packetery\Module\Helper;
-use Packetery\Module\MessageManager;
 use Packetery\Module\Order\PacketSetStoredUntil;
-use Packetery\Module\Order\Repository;
-use Packetery\Nette\Http\Request;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use Tests\Core\DummyFactory;
@@ -20,14 +16,12 @@ class PacketSetStoredUntilTest extends TestCase {
 
 	private Client|MockObject $client;
 	private ILogger|MockObject $loggerMock;
+	private ResponsePacketSetStoredUntil|MockObject $responsePacketSetStoredUntil;
+	private PacketSetStoredUntil $packetSetStoredUntil;
 
-	public function setUp(): void {
+	public function createPacketStoredUntil(): void {
 		$this->client                       = $this->createMock( Client::class );
 		$this->loggerMock                   = $this->createMock( ILogger::class );
-		$this->orderRepositoryMock          = $this->createMock( Repository::class );
-		$this->requestMock                  = $this->createMock( Request::class );
-		$this->messageManagerMock           = $this->createMock( MessageManager::class );
-		$this->helper                       = $this->createMock( Helper::class );
 		$this->responsePacketSetStoredUntil = $this->createMock( ResponsePacketSetStoredUntil::class );
 
 		$this->packetSetStoredUntil = new PacketSetStoredUntil(
@@ -37,6 +31,7 @@ class PacketSetStoredUntilTest extends TestCase {
 	}
 
 	public function testSetStoredUntilWithoutFault(): void {
+		$this->createPacketStoredUntil();
 		$this->responsePacketSetStoredUntil->method( 'hasFault' )->willReturn( false );
 		$this->loggerMock->expects( $this->once() )->method( 'add' );
 
@@ -48,6 +43,7 @@ class PacketSetStoredUntilTest extends TestCase {
 	}
 
 	public function testSetStoredUntilWithFault(): void {
+		$this->createPacketStoredUntil();
 		$errorMessage = 'some nasty fault';
 
 		$this->responsePacketSetStoredUntil->method( 'hasFault' )->willReturn( true );
