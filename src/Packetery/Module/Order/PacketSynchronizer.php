544885d343e9659a94e4117d10bd7d0b8a1abac6
Adam Karpita <adam.karpita@packeta.com>
2024-11-01 14:43:36 +0100

    fixup! PES-2318: Add additional packet status codes

--- src/Packetery/Module/Order/PacketSynchronizer.php
+++ src/Packetery/Module/Order/PacketSynchronizer.php
@@ -179,7 +179,7 @@ class PacketSynchronizer {
 			PacketStatus::CUSTOMS                =>
 				new PacketStatus( PacketStatus::CUSTOMS, __( 'Customs declaration process', 'packeta' ), true ),
 			PacketStatus::REVERSE_PACKET_ARRIVED =>
-				new PacketStatus( PacketStatus::REVERSE_PACKET_ARRIVED, __( 'Reverse parcel has been accepted at our branch', 'packeta' ), true ),
+				new PacketStatus( PacketStatus::REVERSE_PACKET_ARRIVED, __( 'Reverse parcel has been accepted at our pick up point', 'packeta' ), true ),
 			PacketStatus::DELIVERY_ATTEMPT       =>
 				new PacketStatus( PacketStatus::DELIVERY_ATTEMPT, __( 'Unsuccessful delivery attempt of parcel', 'packeta' ), true ),
 			PacketStatus::REJECTED_BY_RECIPIENT  =>
