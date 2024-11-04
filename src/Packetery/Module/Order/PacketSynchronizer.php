f7b8e9a8bc24b336eaf2b7004dbfd9192709f8b8
Jan Potužník <jan.potuznik@gmail.com>
2024-11-04 12:42:07 +0100

    PES-2321: namespace spacing unified

--- src/Packetery/Module/Order/PacketSynchronizer.php
+++ src/Packetery/Module/Order/PacketSynchronizer.php
@@ -7,7 +7,6 @@
 
 declare( strict_types=1 );
 
-
 namespace Packetery\Module\Order;
 
 use Packetery\Core\Api;
--- src/Packetery/Module/Product/DataTab.php
+++ src/Packetery/Module/Product/DataTab.php
@@ -7,7 +7,6 @@
 
 declare( strict_types=1 );
 
-
 namespace Packetery\Module\Product;
 
 use Packetery\Latte\Engine;
