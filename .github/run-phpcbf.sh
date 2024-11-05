1efac974c7da2344728f82b3867c1277fc4b2bb5
Jan Potužník <jan.potuznik@gmail.com>
2024-11-05 08:22:59 +0100

    PES-2321: phpcbf pipeline update

--- .github/run-phpcbf.sh
+++ .github/run-phpcbf.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-# Run phpcbf and capture output
-output=$(./vendor/bin/phpcbf "$@")
+./vendor/bin/phpcbf
 exit_code=$?
-
-echo $output
 
 # Check if phpcbf made changes (non-zero exit code indicates fixes were made)
 if [ $exit_code -ne 0 ]; then
-  echo "phpcbf made changes to the code."
+  echo "🛑 phpcbf made changes to the code."
   exit 1
 else
-  echo "No changes were made by phpcbf."
+  echo "✓ no changes were made by phpcbf."
   exit 0
 fi
