f33acda38cdfbcca91e1b8124f0e3698591d711b
Jan Potužník <jan.potuznik@gmail.com>
2024-11-05 10:33:42 +0100

    PES-2321: check:ec fix

--- .github/run-phpcbf.sh
+++ .github/run-phpcbf.sh
@@ -5,9 +5,9 @@ exit_code=$?
 
 # Check if phpcbf made changes (non-zero exit code indicates fixes were made)
 if [ $exit_code -ne 0 ]; then
-  echo "🛑 phpcbf made changes to the code."
-  exit 1
+	echo "🛑 phpcbf made changes to the code."
+	exit 1
 else
-  echo "✓ no changes were made by phpcbf."
-  exit 0
+	echo "✓ no changes were made by phpcbf."
+	exit 0
 fi
