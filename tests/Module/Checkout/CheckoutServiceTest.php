a9d54a5a07bf0abf0a165dde79dfbae3f3cb9d77
Jan Potužník <jan.potuznik@gmail.com>
2024-12-02 14:53:52 +0100

    PES-2337: Covering with tests

--- tests/Module/Checkout/CheckoutServiceTest.php
+++ tests/Module/Checkout/CheckoutServiceTest.php
@@ -11,10 +11,12 @@ use Packetery\Module\Checkout\CheckoutService;
 use Packetery\Module\Framework\WcAdapter;
 use Packetery\Module\Framework\WpAdapter;
 use Packetery\Module\Options\OptionsProvider;
+use Packetery\Module\ShippingMethod;
 use Packetery\Nette\Http\Request;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use Tests\Module\MockFactory;
+use WC_Shipping_Rate;
 
 class CheckoutServiceTest extends TestCase {
 
@@ -31,17 +33,55 @@ class CheckoutServiceTest extends TestCase {
 		$this->carrierEntityRepository = $this->createMock( Carrier\EntityRepository::class );
 		$this->carDeliveryConfig       = $this->createMock( CarDeliveryConfig::class );
 		$this->provider                = $this->createMock( OptionsProvider::class );
+		$this->httpRequest             = $this->createMock( Request::class );
 
 		$this->resolver = new CheckoutService(
 			$this->wpAdapter,
 			$this->wcAdapter,
-			$this->createMock( Request::class ),
+			$this->httpRequest,
 			$this->carDeliveryConfig,
 			$this->createMock( Carrier\Repository::class ),
 			$this->carrierEntityRepository,
 			$this->createMock( PacketaPickupPointsConfig::class ),
 			$this->provider,
 		);
+	}
+
+	public function testCalculateShippingAndGetIdReturnsEmptyStringWhenNoShippingRatesExists(): void {
+		$this->createCheckoutServiceMock();
+
+		$this->wcAdapter->method( 'cartCalculateShipping' )->willReturn( [] );
+		$this->assertEquals( '', $this->resolver->calculateShippingAndGetId() );
+	}
+
+	public function testCalculateShippingAndGetIdReturnsShippingRateIdStrippedOfPrefix(): void {
+		$this->createCheckoutServiceMock();
+
+		$shippingRateId     = 'packetery_carrier_zpointcz';
+		$shippingRateFullId = ShippingMethod::PACKETERY_METHOD_ID . ':' . $shippingRateId;
+		$mockedShippingRate = $this->createMock( WC_Shipping_Rate::class );
+		$mockedShippingRate->method( 'get_id' )->willReturn( $shippingRateFullId );
+		$this->wcAdapter->method( 'cartCalculateShipping' )->willReturn( [ $mockedShippingRate ] );
+
+		$this->assertEquals( $shippingRateId, $this->resolver->calculateShippingAndGetId() );
+	}
+
+	public function testGetChosenMethodWhenPostShippingMethodIsNull(): void {
+		$this->createCheckoutServiceMock();
+
+		$this->httpRequest->method( 'getPost' )->with( 'shipping_method' )->willReturn( null );
+
+		$this->assertEquals( '', $this->resolver->getChosenMethod() );
+	}
+
+	public function testGetChosenMethodWhenPostShippingMethodIsNotNull(): void {
+		$this->createCheckoutServiceMock();
+
+		$shippingRateId     = 'packetery_carrier_zpointcz';
+		$shippingRateFullId = ShippingMethod::PACKETERY_METHOD_ID . ':' . $shippingRateId;
+		$this->httpRequest->method( 'getPost' )->with( 'shipping_method' )->willReturn( [ $shippingRateFullId ] );
+
+		$this->assertEquals( $shippingRateId, $this->resolver->getChosenMethod() );
 	}
 
 	public function testAreBlocksUsedInCheckoutBlockDetection(): void {
