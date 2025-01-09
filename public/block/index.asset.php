172e763986e2b765f282aceff5388a197f3eca39
Jan Potužník <jan.potuznik@gmail.com>
2025-01-09 09:48:05 +0100

    PES-2426: checkout block compiled after merge

--- public/block/index.asset.php
+++ public/block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-checkout', 'wc-blocks-components', 'wc-settings', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-i18n'), 'version' => '59f7ca168fc7d6a0030c');
+<?php return array('dependencies' => array('react', 'wc-blocks-checkout', 'wc-blocks-components', 'wc-settings', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-i18n'), 'version' => '6367f2dccc8355364141');
