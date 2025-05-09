911343ac5795f95fa9760caa5ccac62d5f9df493
J7F <J7F@gmail.com>
2025-05-09 10:45:56 +0200

    PES-2695: Revolut pay by card fix - test added

---
+++ tests/Module/Checkout/ShippingTaxModifierTest.php
@@ -0,0 +1,89 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Module\Checkout;
+
+use Packetery\Module\Checkout\ShippingTaxModifier;
+use Packetery\Module\Framework\WpAdapter;
+use Packetery\Module\Options\OptionsProvider;
+use PHPUnit\Framework\TestCase;
+use WC_Order_Item_Shipping;
+
+class ShippingTaxModifierTest extends TestCase {
+
+	private WpAdapter|MockObject $wpAdapterMock;
+	private OptionsProvider|MockObject $optionsProviderMock;
+	private ShippingTaxModifier $shippingTaxModifier;
+
+	protected function setup(): void {
+		$this->wpAdapterMock       = $this->createMock( WpAdapter::class );
+		$this->optionsProviderMock = $this->createMock( OptionsProvider::class );
+
+		$this->shippingTaxModifier = new ShippingTaxModifier(
+			$this->wpAdapterMock,
+			$this->optionsProviderMock
+		);
+	}
+
+	public function testRegister(): void {
+		$this->wpAdapterMock
+			->expects( $this->once() )
+			->method( 'addAction' )
+			->with(
+				'woocommerce_order_item_shipping_after_calculate_taxes',
+				[ $this->shippingTaxModifier, 'afterCalculateTaxes' ]
+			);
+
+		$this->shippingTaxModifier->register();
+	}
+
+	public function testAfterCalculateTaxesSkipsWhenPricesAreNotTaxInclusive(): void {
+		$this->optionsProviderMock
+			->method( 'arePricesTaxInclusive' )
+			->willReturn( false );
+
+		$shippingItemMock = $this->createMock( WC_Order_Item_Shipping::class );
+		$shippingItemMock->expects( $this->never() )->method( 'get_method_id' );
+
+		$this->shippingTaxModifier->afterCalculateTaxes( $shippingItemMock );
+	}
+
+	public function testAfterCalculateTaxesSkipsWhenMethodIsNotPacketa(): void {
+		$this->optionsProviderMock
+			->method( 'arePricesTaxInclusive' )
+			->willReturn( true );
+
+		$shippingItemMock = $this->createMock( WC_Order_Item_Shipping::class );
+		$shippingItemMock
+			->method( 'get_method_id' )
+			->willReturn( 'non_packeta_method' );
+
+		$shippingItemMock->expects( $this->never() )->method( 'get_meta' );
+
+		$this->shippingTaxModifier->afterCalculateTaxes( $shippingItemMock );
+	}
+
+	public function testAfterCalculateTaxesSetsTaxes(): void {
+		$this->optionsProviderMock
+			->method( 'arePricesTaxInclusive' )
+			->willReturn( true );
+
+		$shippingItemMock = $this->createMock( WC_Order_Item_Shipping::class );
+		$shippingItemMock
+			->method( 'get_method_id' )
+			->willReturn( 'packeta_method_zpointcz' );
+
+		$shippingItemMock
+			->method( 'get_meta' )
+			->with( 'packetaTaxes' )
+			->willReturn( [ 10 ] );
+
+		$shippingItemMock
+			->expects( $this->once() )
+			->method( 'set_taxes' )
+			->with( [ 'total' => [ 10 ] ] );
+
+		$this->shippingTaxModifier->afterCalculateTaxes( $shippingItemMock );
+	}
+}
