bdd866872ae5f1375aa483878211a429e8a582da
J7F <J7F@gmail.com>
2025-08-07 15:52:31 +0200

    PES-2224: requirements update

--- packeta.php
+++ packeta.php
@@ -13,8 +13,8 @@
  * Author URI: https://www.zasilkovna.cz/
  * Text Domain: packeta
  * Domain Path: /languages
- * Requires at least: 5.5
- * Requires PHP: 7.2
+ * Requires at least: 6.3
+ * Requires PHP: 7.4
  * Requires Plugins: woocommerce
  *
  * Tested up to: 6.8.2
