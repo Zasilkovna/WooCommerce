6403179a74ce456bcfcd7160575d04343c7de3c8
Adam Karpita <adam.karpita@packeta.com>
2025-08-21 15:11:02 +0200

    PES-2588: Add `WpAdapter` for translation handling in `Order/Form` class

--- src/Packetery/Module/Order/Form.php
+++ src/Packetery/Module/Order/Form.php
@@ -1,9 +1,4 @@
 <?php
-/**
- * Class Form.
- *
- * @package Packetery
- */
 
 declare( strict_types=1 );
 
@@ -13,14 +8,10 @@ use Packetery\Core\CoreHelper;
 use Packetery\Core\Validator;
 use Packetery\Module\FormFactory;
 use Packetery\Module\FormValidators;
+use Packetery\Module\Framework\WpAdapter;
 use Packetery\Module\Options\OptionsProvider;
 use Packetery\Nette\Forms;
 
-/**
- * Class Form.
- *
- * @package Packetery
- */
 class Form {
 
 	public const FIELD_WEIGHT           = 'packeteryWeight';
@@ -36,18 +27,19 @@ class Form {
 	public const FIELD_DELIVER_ON       = 'packeteryDeliverOn';
 
 	/**
-	 * Class FormFactory
-	 *
 	 * @var FormFactory
 	 */
 	private $formFactory;
 
 	/**
-	 * Class Provider
-	 *
 	 * @var OptionsProvider
 	 */
 	private $options;
+
+	/**
+	 * @var WpAdapter
+	 */
+	private $wpAdapter;
 
 	/**
 	 * FormFactory constructor
@@ -55,45 +47,46 @@ class Form {
 	 * @param FormFactory     $formFactory Form factory.
 	 * @param OptionsProvider $options Options provider.
 	 */
-	public function __construct( FormFactory $formFactory, OptionsProvider $options ) {
+	public function __construct( FormFactory $formFactory, OptionsProvider $options, WpAdapter $wpAdapter ) {
 		$this->formFactory = $formFactory;
 		$this->options     = $options;
+		$this->wpAdapter   = $wpAdapter;
 	}
 
 	public function create(): Forms\Form {
 		$form = $this->formFactory->create();
 		$unit = $this->options->getDimensionsUnit();
 
-		$form->addText( self::FIELD_WEIGHT, __( 'Weight (kg)', 'packeta' ) )
+		$form->addText( self::FIELD_WEIGHT, $this->wpAdapter->__( 'Weight (kg)', 'packeta' ) )
 			->setRequired( false )
 			->setNullable()
 			->addRule( Forms\Form::FLOAT );
 		$form->addHidden( self::FIELD_ORIGINAL_WEIGHT );
-		$this->formFactory->addDimension( $form, self::FIELD_LENGTH, __( 'Length', 'packeta' ), $unit )
+		$this->formFactory->addDimension( $form, self::FIELD_LENGTH, $this->wpAdapter->__( 'Length', 'packeta' ), $unit )
 			->setNullable();
-		$this->formFactory->addDimension( $form, self::FIELD_WIDTH, __( 'Width', 'packeta' ), $unit )
+		$this->formFactory->addDimension( $form, self::FIELD_WIDTH, $this->wpAdapter->__( 'Width', 'packeta' ), $unit )
 			->setNullable();
-		$this->formFactory->addDimension( $form, self::FIELD_HEIGHT, __( 'Height', 'packeta' ), $unit )
+		$this->formFactory->addDimension( $form, self::FIELD_HEIGHT, $this->wpAdapter->__( 'Height', 'packeta' ), $unit )
 			->setNullable();
-		$form->addCheckbox( self::FIELD_ADULT_CONTENT, __( 'Adult content', 'packeta' ) )
+		$form->addCheckbox( self::FIELD_ADULT_CONTENT, $this->wpAdapter->__( 'Adult content', 'packeta' ) )
 			->setRequired( false );
-		$form->addText( self::FIELD_COD, __( 'Cash on delivery', 'packeta' ) )
+		$form->addText( self::FIELD_COD, $this->wpAdapter->__( 'Cash on delivery', 'packeta' ) )
 			->setRequired( false )
 			->setNullable()
 			->addRule( Forms\Form::FLOAT );
 		$form->addHidden( self::FIELD_CALCULATED_COD );
-		$form->addText( self::FIELD_VALUE, __( 'Order value', 'packeta' ) )
+		$form->addText( self::FIELD_VALUE, $this->wpAdapter->__( 'Order value', 'packeta' ) )
 			->setRequired( false )
 			->setNullable()
 			->addRule( Forms\Form::FLOAT );
 		$form->addHidden( self::FIELD_CALCULATED_VALUE );
-		$form->addText( self::FIELD_DELIVER_ON, __( 'Planned dispatch', 'packeta' ) )
+		$form->addText( self::FIELD_DELIVER_ON, $this->wpAdapter->__( 'Planned dispatch', 'packeta' ) )
 			->setHtmlAttribute( 'class', 'date-picker' )
 			->setHtmlAttribute( 'autocomplete', 'off' )
 			->setRequired( false )
 			->setNullable()
 			// translators: %s: Represents minimal date for delayed delivery.
-			->addRule( [ FormValidators::class, 'dateIsLater' ], __( 'Date must be later than %s', 'packeta' ), wp_date( CoreHelper::DATEPICKER_FORMAT ) );
+			->addRule( [ FormValidators::class, 'dateIsLater' ], $this->wpAdapter->__( 'Date must be later than %s', 'packeta' ), wp_date( CoreHelper::DATEPICKER_FORMAT ) );
 
 		return $form;
 	}
