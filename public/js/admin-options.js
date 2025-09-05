ad27ab68ddda7d665bb3d584a3484c640c01117e
Adam Karpita <adam.karpita@packeta.com>
2025-09-05 13:01:31 +0200

    PES-2937: Add DiagnosticsLogger to handle log, log deletion and integrate with support tab

--- public/js/admin-options.js
+++ public/js/admin-options.js
@@ -3,5 +3,12 @@
 		$( '[data-packetery-select2]' ).select2({
 			dropdownParent: $('.custom-select2-wrapper')
 		});
+
+		$('.js-packetery-delete-log').on('click', function(e) {
+			if (!confirm(translationsAdminOptions.confirmLogDeletion)) {
+				e.preventDefault();
+				return false;
+			}
+		});
 	});
 })(jQuery);
