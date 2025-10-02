86392d7dd6f15ad8e994a1492dc3bb9a38ac0ec2
Jan Potužník <jan.potuznik@gmail.com>
2025-10-02 12:57:04 +0200

    PES-2975: removed feature flag downloader and related code

--- src/Packetery/Module/Options/FlagManager/FeatureFlagProvider.php
+++ src/Packetery/Module/Options/FlagManager/FeatureFlagProvider.php
@@ -5,10 +5,6 @@ declare( strict_types=1 );
 namespace Packetery\Module\Options\FlagManager;
 
 class FeatureFlagProvider {
-
-	public const FLAG_SPLIT_ACTIVE  = 'splitActive';
-	public const FLAG_LAST_DOWNLOAD = 'lastDownload';
-
 	public function isSplitActive(): bool {
 		// Enabled for all users. Method will be removed later.
 		return true;
