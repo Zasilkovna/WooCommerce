18f7cb4448df97ec1b80e92daf328a27d503478e
Jaroslav Malý <jarda.maly@gmail.com>
2026-07-01 10:08:38 +0200

    PES-3266: add newly generated carrier classes

---
+++ src/Packetery/Module/Shipping/Generated/ShippingMethod_272.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * This file is auto-generated.
+ */
+
+declare(strict_types=1);
+
+namespace Packetery\Module\Shipping\Generated;
+
+use Packetery\Module\Shipping\BaseShippingMethod;
+
+/**
+ * PL Polish Post 48 HD
+ */
+class ShippingMethod_272 extends BaseShippingMethod
+{
+	public const CARRIER_ID = '272';
+}
